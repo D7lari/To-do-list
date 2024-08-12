8460a2eaa7e31630cfed52b82a0fc839ccc3a13a
Larissa Araujo Silva <larissa0643@digitalcollegesul.local>
2024-08-12 12:05:45 -0300

    atividade

--- js/index.js
+++ js/index.js
@@ -1,13 +1,76 @@
 function novaTarefa(){
-    overlay.classList.add("active")
+    overlay.classList.add("active");
+    modal.classList.add("active");
+    criar.classList.add("active");
+}
+
+function confirmarExclusao(){
+    overlay.classList.add("active");
+    modal.classList.add("active");
+    confirmacao.classList.add("active");
 }
 
 function fecharModal(){
-    overlay.classList.remove("active")
+    overlay.classList.remove("active");
+    modal.classList.remove("active");
+    criar.classList.remove("active");
+    confirmacao.classList.remove("active");
 }
 
-document.addEventListener('keyup',(evento) =>{
+document.addEventListener('keyup', (evento) => {
     if(evento.key == "Escape"){
         fecharModal();
+    }   
+});
+
+let tarefas = [
+    {
+        titulo: "Pendurar quadro",
+        descricao: "Furar dois furos na parede para colocar as buchas e pendurar o quadro"
+    },
+    {
+        titulo: "Comer",
+        descricao: "Em casa"
+    },
+    {
+        titulo: "Reunião com cliente",
+        descricao: "Ir na KF veiculos se reunir com fabricio"
+    },
+    {
+        titulo: "Estudar",
+        descricao: "Entender melhor variaveis e constantes"
+    },
+    {
+        titulo: "Comprar capacete",
+        descricao: "Ir na loja e comprar o capacete que EU já escolhi e não ceder ao consumismo"
+    },
+];
+
+function listarTarefas(){
+    lista.innerHTML = "";
+    tarefas.map((tarefa) => {
+        lista.innerHTML += `
+         <li>
+                <div>
+                    <h5>${tarefa.titulo}</h5>
+                    <p>${tarefa.descricao}</p>
+                </div>
+                <div>
+                    <box-icon name='trash' onclick="confirmarExclusao()"></box-icon>
+                </div>
+            </li>
+        `;
+    })
+}
+listarTarefas();
+
+function adicionarTarefa(){
+    event.preventDefault();
+    let tarefa = {
+        titulo: titulo.value,
+        descricao: descricao.value
     }
-})
+    tarefas.push(tarefa)
+    fecharModal();
+    listarTarefas();
+}
